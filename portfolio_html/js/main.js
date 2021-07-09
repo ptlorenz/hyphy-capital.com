9000a59818be9523c19e7542af9f32caa4aa4bd2
Patrick Lorenz <61086523+ptlorenz@users.noreply.github.com>
2021-07-09 17:07:36 +0200

    Delete portfolio_html directory

--- portfolio_html/js/main.js
+++
@@ -1,8 +0,0 @@
-import TableCsv from "./TableCsv.js";
-
-const tableRoot = document.querySelector ("#csvRoot");
-const tableCsv = new TableCsv (tableRoot);
-
-tableCsv.update([
-  [4500, "dom", 200]
-],["ID","Name","Age"] );
